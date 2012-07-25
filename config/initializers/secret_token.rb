d6a6ae70e673c8a4ddcf7f30deac95bffd3decbd
Reuben Mallaby <reuben.mallaby@gmail.com>
2012-07-25 13:17:06 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComicBook::Application.config.secret_token = '63091cff8ff9dce0a7f230f2b6081101ed7e8b536b3c1ddfa1b99fe6a66a12818361cdf3c2774257c438dec62963ae38fe25e989f36817ad40b9d20d114702c5'

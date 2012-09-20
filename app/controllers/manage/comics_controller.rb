efd62913384d31008b5ca12fa31b136b9edb2fb9
= <=>
2012-09-20 10:26:42 +0200

    Added check for facebook post errors

--- app/controllers/manage/comics_controller.rb
+++ app/controllers/manage/comics_controller.rb
@@ -29,33 +29,21 @@ class Manage::ComicsController < Manage::BaseController
     if @comic.save
 
       if Settings.uses_facebook
-        #post
-        #page = FbGraph::Page.new(Settings.facebook_page)
-        #logger.info ">>>> PAGE #{page}"
-        #logger.info ">>>> FEED #{page.feed}"
-        #page.feed!(
-        #  :message => @comic.name,
-        #  #:picture => "http://comicbook.mallaby.me/#{@comic.image.url(:original)}",
-        #  :link => archived_url(@comic.publish_date.year, @comic.publish_date.month, @comic.publish_date.day),
-        #  #:name => @comic.name,
-        #  #:description => @comic.description
-        #)
-        koala_page = Koala::Facebook::API.new(Settings.facebook_page_access_token)
-        #koala_user.get_connections('me', 'accounts')
-        #render :text => koala_user.get_connections('me', 'accounts')
-
-        #logger.info ">>>> pic url = http://comicbook.mallaby.me#{@comic.image.url(:original)}"
-        #logger.info ">>>> link url = #{archived_url(@comic.publish_date.year, @comic.publish_date.month, @comic.publish_date.day)}"
-
-        koala_page.put_connections(Settings.facebook_page, 'feed', :message => "New image - #{@comic.name}", :picture => "http://comicbook.mallaby.me#{@comic.image.url(:original)}", :link => archived_url(@comic.publish_date.year, @comic.publish_date.month, @comic.publish_date.day))
-
+        begin
+          koala_page = Koala::Facebook::API.new(Settings.facebook_page_access_token)
+          koala_page.put_connections(Settings.facebook_page, 'feed', :message => "New image - #{@comic.name}", :picture => "http://comicbook.mallaby.me#{@comic.image.url(:original)}", :link => archived_url(@comic.publish_date.year, @comic.publish_date.month, @comic.publish_date.day))
+        rescue
+          #TODO
+          #Notify user that they need to update their access tokens
+        end
       end
+
 
       if Settings.uses_twitter
         #post
       end
 
-      #redirect_to manage_comics_url, notice: 'Comic was successfully created.'
+      redirect_to manage_comics_url, notice: 'Comic was successfully created.'
     else
       @books = Book.available
       @tags = Tag.all

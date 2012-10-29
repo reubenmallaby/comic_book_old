1ee3c680c96e7389a87c4b5705f9593b9e8a1329
= <=>
2012-10-29 18:57:06 +0100

    Removed fblikebox from social partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,10 @@ class ApplicationController < ActionController::Base
   #include Clearance::Authentication
   protect_from_forgery
 
+  protected
+
+  def authorize
+    current_user && current_user.admin
+  end
 
 end

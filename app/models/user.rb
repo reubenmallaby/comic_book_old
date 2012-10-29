92888e2d80e321ab402ccecb11232e20307e8087
= <=>
2012-10-29 17:14:21 +0100

    Added FB check to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,17 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :email
   validates_uniqueness_of :name, :email
 
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+      )
+    end
+    user
+  end
+
 end

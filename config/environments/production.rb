e22caf03e9b50c09f28c0d491a25face7059ac13
= <=>
2012-10-29 17:11:28 +0100

    Fixed example URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ ComicBook::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'www.mycomic.com' }
+  config.action_mailer.default_url_options = { :host => 'comicbook.mallaby.me' }
 
 end

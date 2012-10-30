c919624ea966ea753923595ba1a8c9446821d95f
= <=>
2012-10-30 14:27:48 +0100

    Modified comic pager links

--- app/helpers/comics_helper.rb
+++ app/helpers/comics_helper.rb
@@ -1,3 +1,13 @@
 module ComicsHelper
 
+  def archived_comic_path comic
+    return "#" unless comic
+    archived_path(comic.publish_date.year, comic.publish_date.month, comic.publish_date.day)
+  end
+
+  def archived_comic_url comic
+    return "#" unless comic
+    archived_url(comic.publish_date.year, comic.publish_date.month, comic.publish_date.day)
+  end
+
 end
